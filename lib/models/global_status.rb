d72c0910a2120976096583ef4f39dda7d0a71d35
Adam Workman <adammworkman25@gmail.com>
2020-07-08 16:30:21 -0400

    minor refactor

--- lib/models/global_status.rb
+++ lib/models/global_status.rb
@@ -21,12 +21,12 @@ class GlobalStatus
 
     def self.global_stats
         
-        puts "        New confirmed cases:".yellow + "        #{@@all[0].new_confirmed}\n".red
-        puts "        Total confrimed cases:".yellow + "      #{@@all[0].total_confirmed}\n".red
-        puts "        Number of new deaths:".yellow + "       #{@@all[0].new_deaths}\n".red
-        puts "        Total number of deaths:".yellow + "     #{@@all[0].total_deaths}\n".red
-        puts "        Number of new recoveries:".yellow + "   #{@@all[0].new_recovered}\n".red
-        puts "        Total number of recoveries:".yellow + " #{@@all[0].total_recovered}".red
+        puts "        New confirmed cases:".yellow + "        #{all[0].new_confirmed}\n".red
+        puts "        Total confrimed cases:".yellow + "      #{all[0].total_confirmed}\n".red
+        puts "        Number of new deaths:".yellow + "       #{all[0].new_deaths}\n".red
+        puts "        Total number of deaths:".yellow + "     #{all[0].total_deaths}\n".red
+        puts "        Number of new recoveries:".yellow + "   #{all[0].new_recovered}\n".red
+        puts "        Total number of recoveries:".yellow + " #{all[0].total_recovered}".red
 
         
     end
